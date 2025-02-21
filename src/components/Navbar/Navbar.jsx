3f02908a4c795912bc06f6ae798f3dfe34b436a6
Anwar_Hossain <ranakrphone@gmail.com>
2025-02-21 12:13:46 +0600

    navbar updated

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -4,14 +4,16 @@ import { AuthContext } from "../../providers/AuthProvider";
 
 const Navbar = () => {
   const { user, logOutUser } = useContext(AuthContext);
+
   return (
-    <div className="navbar bg-base-100">
+    <div className="navbar bg-[#f8fdfd] shadow-lg px-4 md:px-8">
+      {/* Left Side (Logo & Mobile Menu) */}
       <div className="navbar-start">
         <div className="dropdown">
-          <div tabIndex={0} role="button" className="btn btn-ghost lg:hidden">
+          <label tabIndex={0} className="btn btn-ghost lg:hidden">
             <svg
               xmlns="http://www.w3.org/2000/svg"
-              className="h-5 w-5"
+              className="h-6 w-6 text-[#040d0d]"
               fill="none"
               viewBox="0 0 24 24"
               stroke="currentColor"
@@ -23,69 +25,67 @@ const Navbar = () => {
                 d="M4 6h16M4 12h8m-8 6h16"
               />
             </svg>
-          </div>
+          </label>
           <ul
             tabIndex={0}
-            className="menu menu-sm dropdown-content bg-base-100 rounded-box z-[1] mt-3 w-52 p-2 shadow"
+            className="menu menu-sm dropdown-content bg-white rounded-box z-[1] mt-3 w-52 p-2 shadow-lg"
           >
             <li>
-              <a>Item 1</a>
-            </li>
-            <li>
-              <a>Parent</a>
-              <ul className="p-2">
-                <li>
-                  <a>Submenu 1</a>
-                </li>
-                <li>
-                  <a>Submenu 2</a>
-                </li>
-              </ul>
-            </li>
-            <li>
-              <a>Item 3</a>
+              <Link
+                to="/"
+                className="text-[#040d0d] hover:bg-[#9aade3] rounded"
+              >
+                Home
+              </Link>
             </li>
           </ul>
         </div>
-        <Link to={"/"} className="btn btn-ghost text-xl">
-          daisyUI
+        <Link to="/" className="text-2xl font-bold text-[#040d0d]">
+          Task Manager
         </Link>
       </div>
+
+      {/* Center Menu (Desktop View) */}
       <div className="navbar-center hidden lg:flex">
-        <ul className="menu menu-horizontal px-1">
+        <ul className="menu menu-horizontal space-x-4">
           <li>
-            <a>Item 1</a>
-          </li>
-          <li>
-            <details>
-              <summary>Parent</summary>
-              <ul className="p-2">
-                <li>
-                  <a>Submenu 1</a>
-                </li>
-                <li>
-                  <a>Submenu 2</a>
-                </li>
-              </ul>
-            </details>
-          </li>
-          <li>
-            <a>Item 3</a>
+            <Link to="/" className="text-[#040d0d] hover:text-[#6e78d7]">
+              Home
+            </Link>
           </li>
         </ul>
       </div>
-      <div className="navbar-end">
-        {user && (
-          <>
-            <h1>{user.displayName}</h1>
-            <button onClick={() => logOutUser()} className="btn">
+
+      {/* Right Side (User Profile & Logout) */}
+      <div className="navbar-end space-x-4">
+        {user ? (
+          <div className="flex items-center space-x-3">
+            <div className="flex items-center space-x-2">
+              <img
+                referrerPolicy="no-referrer"
+                src={user?.photoURL}
+                alt={user?.displayName}
+                className="w-10 h-10 rounded-full border-2 border-[#43cbc3] shadow-sm"
+              />
+              <span className="text-[#040d0d] font-semibold">
+                {user?.displayName}
+              </span>
+            </div>
+            <button
+              onClick={logOutUser}
+              className="btn bg-[#43cbc3] text-white hover:bg-[#36a39a] transition"
+            >
               Logout
             </button>
-          </>
+          </div>
+        ) : (
+          <Link
+            to="/login"
+            className="btn bg-[#6e78d7] text-white hover:bg-[#5a62c0] transition"
+          >
+            Login
+          </Link>
         )}
-        <Link to={"/login"} className="btn">
-          Login
-        </Link>
       </div>
     </div>
   );
